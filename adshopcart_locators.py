a0e0bbe73115021ac8eb1f6bd3329fcdef819e16
Kevin <garcia.kevin220@gmail.com>
2022-03-01 18:30:01 -0800

    locators_v2

--- adshopcart_locators.py
+++ adshopcart_locators.py
@@ -1,1 +1,23 @@
+from faker import Faker
+
+fake = Faker(locale='en_CA')
 adshopcart_url = 'https://advantageonlineshopping.com/#/'
+cartpage_url = 'https://advantageonlineshopping.com/#/shoppingCart'
+
+old_username = fake.user_name()
+new_username = old_username[0:11]
+
+email = fake.email()
+new_password = fake.password()
+first_name = fake.first_name()
+last_name = fake.last_name()
+full_name = f'{first_name} {last_name}'
+phone = fake.phone_number()
+city = fake.city()
+
+address = fake.street_address()
+
+province = fake.province_abbr()
+postalcode = fake.postalcode()
+
+
