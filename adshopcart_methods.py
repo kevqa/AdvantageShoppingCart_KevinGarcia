bf3eedf768cda208034f88fd95c2da4b80d9bedf
Kevin <garcia.kevin220@gmail.com>
2022-03-02 12:27:24 -0800

    methods_v3

--- adshopcart_methods.py
+++ adshopcart_methods.py
@@ -104,11 +104,11 @@ def delete_account_retry_login():
     driver.find_element(By.ID, 'menuUser').click()
     sleep(1)
     driver.find_element(By.XPATH, "//*[@id='loginMiniTitle']//label[text()='My account']").click()
-    sleep(2)
+    sleep(4)
     driver.find_element(By.CLASS_NAME, 'deleteBtnText').click()
     sleep(2)
     driver.find_element(By.XPATH, '//div[text()="yes"]').click()
-    sleep(5)
+    sleep(8)
     driver.find_element(By.ID, 'menuUser').click()
     sleep(1)
     driver.find_element(By.NAME, 'username').send_keys(locators.new_username)
@@ -122,6 +122,87 @@ def delete_account_retry_login():
         print(f'Account no longer accepted. Test passed')
     else:
         print(f'Error found. Check code.')
+    sleep(1)
+    driver.refresh()
+    sleep(2)
+
+def product_check():
+    if driver.page_source.find('SPEAKERS'):
+        print(f'------------------------------------------')
+        print(f'Hey SPEAKERS!')
+    else:
+        print(f'Check code, please.')
+    sleep(1)
+    if driver.page_source.find('TABLETS'):
+        print(f'Hey TABLETS!')
+    else:
+        print(f'Check code, please.')
+    sleep(1)
+    if driver.page_source.find('LAPTOPS'):
+        print(f'Hey LAPTOPS!')
+    else:
+        print(f'Check code, please.')
+    sleep(1)
+    if driver.page_source.find('MICE'):
+        print(f'Hey MICE!')
+    else:
+        print(f'Check code, please.')
+    sleep(1)
+    if driver.page_source.find('HEADPHONES'):
+        print(f'Hey HEADPHONES!')
+    else:
+        print(f'Check code, please.')
+    sleep(1)
+
+def top_checks():
+    driver.find_element(By.LINK_TEXT, 'CONTACT US').click()
+    print(f'------------------------------------------')
+    print(f'Moving to CONTACT US.')
+    sleep(2)
+    driver.find_element(By.LINK_TEXT, 'POPULAR ITEMS').click()
+    print(f'Moving to POPULAR ITEMS.')
+    sleep(2)
+    driver.find_element(By.LINK_TEXT, 'SPECIAL OFFER').click()
+    print(f'Moving to SPECIAL OFFER.')
+    sleep(2)
+    driver.find_element(By.LINK_TEXT, 'OUR PRODUCTS').click()
+    print(f'Moving to OUR PRODUCTS.')
+    sleep(2)
+    if driver.page_source.find('dvantage'):
+        print(f'------------------------------------------')
+        print(f'Logo...dvantage, detected!')
+    else:
+        print(f'Check code, please.')
+    sleep(1)
+    if driver.page_source.find('DEMO'):
+        print(f'Logo...DEMO, detected!')
+    else:
+        print(f'Check code, please.')
+    sleep(2)
+
+def contact_section():
+    driver.find_element(By.LINK_TEXT, 'CONTACT US').click()
+    print(f'------------------------------------------')
+    print(f'Returning back to CONTACT US.')
+    sleep(2)
+    Select(driver.find_element(By.XPATH, "//*[@name='categoryListboxContactUs']")).select_by_visible_text('Laptops')
+    sleep(2)
+    Select(driver.find_element(By.XPATH, "//*[@name='productListboxContactUs']")).select_by_visible_text('HP Chromebook 14 G1(ENERGY STAR)')
+    sleep(2)
+    driver.find_element(By.NAME, 'emailContactUs').send_keys(locators.email)
+    sleep(2)
+    driver.find_element(By.NAME, 'subjectTextareaContactUs').send_keys(locators.description)
+    sleep(2)
+    driver.find_element(By.ID, 'send_btnundefined').click()
+    sleep(2)
+    if driver.page_source.find('SPEAKERS'):
+        print(f'Message has been delivered.')
+        print(f'Continue Shopping button is activated')
+    else:
+        print(f'Error in code. Check again.')
+    sleep(2)
+
+
 
 #----------------------------------------------------------------------------------------------------------------------#
 setUp()
@@ -129,5 +210,8 @@ create_new_user()
 name_and_cart()
 log_out_log_in()
 delete_account_retry_login()
+product_check()
+top_checks()
+contact_section()
 tearDown()
 
